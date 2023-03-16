ccc4e0f37a10290f810e9f69cac4b478ad417e89
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-03-16 22:11:11 +0200

    add some data to menuItems dynamics to header

---
+++ src/mock/menuItem.js
@@ -0,0 +1,31 @@
+export const menuItems = [
+  {
+    title: "Find Work",
+    submenu: [
+      {
+        title: "Find Work",
+        url: "/home",
+      },
+      {
+        title: "Saved Jobs",
+        url: "savedJob",
+      },
+      {
+        title: "Profile",
+        url: "/profile",
+      },
+    ],
+  },
+  {
+    title: "My Jobs",
+    url: "/services",
+  },
+  {
+    title: "Reports",
+    url: "/about",
+  },
+  {
+    title: "Messages",
+    url: "/services",
+  },
+];
