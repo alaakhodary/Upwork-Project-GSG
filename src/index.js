541a4c406e49ca18eb3046b1eca21ef8e0066a1f
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-03-16 21:24:16 +0200

    add AuthProvi and Brouser router and ErrorBoundary

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
+
+import ErrorBoundary from "./components/ErrorBoundary";
+import AuthContextProvider from "./contexts/AuthContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ErrorBoundary>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
