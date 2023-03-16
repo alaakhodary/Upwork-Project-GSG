7ab171eb63d607145d433907517addb5077097fc
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-03-16 21:39:51 +0200

    add Header component

---
+++ src/components/Header/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const DivFlex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+  padding: 20px;
+`;
+export const DivLastContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  margin-left: -1rem;
+`;
+export const LogOut = styled.button`
+  height: 3rem;
+  width: 5rem;
+  padding: 5px;
+  border: none;
+  border-radius: 6px;
+  font-size: 16px;
+  font-weight: 700;
+  color: green;
+  cursor: pointer;
+  background-color: whitesmoke;
+`;
